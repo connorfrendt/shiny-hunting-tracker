7b744d2fe2bd8c3db5ce67ca30707c27c9a97bff
Connor Frendt <cjacksonfx@yahoo.com>
2021-11-02 20:11:23 -0700

    short work around for displaying picture/counter/counter buttons only when the search button is pressed. WIP

--- app/src/components/Search.vue
+++ app/src/components/Search.vue
@@ -4,8 +4,15 @@
             <input v-model="pokemonKeyword" />
             <button>SEARCH</button>
         </form>
+        <div v-show="shinySpriteURL.length > 1">
+            <img :src="shinySpriteURL" />
 
-        <img :src="shinySpriteURL" />
+            <div>{{ counter }}</div>
+
+            <button @click="counterUp">+</button>
+            <button @click="counterDown">-</button>
+
+        </div>
     </div>
 </template>
 
@@ -14,17 +21,35 @@ export default {
     data() {
         return {
             pokemonKeyword: '',
-            shinySpriteURL: ''
+            shinySpriteURL: '',
+            counter: 0,
+            pokemons: []
         }
     },
     methods: {
         pokemonAPI() {
-            return fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonKeyword}`)
+            let chosenPokemon = {
+                counter: 0
+            };
+            return fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonKeyword.toLowerCase()}`)
                 .then(response => response.json())
                 .then(data => {
+                    console.log(data);
+                    chosenPokemon.name = data.name;
+                    console.log(chosenPokemon);
                     this.shinySpriteURL = data.sprites.front_shiny;
+
+                    this.pokemons.push(chosenPokemon);
+                    console.log(this.pokemons);
                 })
                 .catch(err => console.log(err));
+        }
+        ,
+        counterUp() {
+            return this.counter++;
+        },
+        counterDown() {
+            return this.counter--;
         }
     }
 }
