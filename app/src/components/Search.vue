5746c080c17bcfe8a0d2d4d83d82d088a25a7fed
Connor Frendt <cjacksonfx@yahoo.com>
2021-11-02 19:14:28 -0700

    hit api, returns shiny sprite

---
+++ app/src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <form @submit.prevent="pokemonAPI">
+            <input v-model="pokemonKeyword" />
+            <button>SEARCH</button>
+        </form>
+
+        <img :src="shinySpriteURL" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pokemonKeyword: '',
+            shinySpriteURL: ''
+        }
+    },
+    methods: {
+        pokemonAPI() {
+            return fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonKeyword}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.shinySpriteURL = data.sprites.front_shiny;
+                })
+                .catch(err => console.log(err));
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
